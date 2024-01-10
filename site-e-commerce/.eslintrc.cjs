ae30a68dfd6ed3fca78c9c4d8e7d15c9f38d1e5d
Escanor1986 <lionel.zovi@gmail.com>
2024-01-10 16:07:31 +0100

    added test on base App.tsx & try to initiate CI with workflow on github

--- site-e-commerce/.eslintrc.cjs
+++ site-e-commerce/.eslintrc.cjs
@@ -4,15 +4,21 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-hooks', 'react-refresh'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
