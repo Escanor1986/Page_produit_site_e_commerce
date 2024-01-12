ce113844f286234280e9e6ce3b6743263ced0415
Escanor1986 <lionel.zovi@gmail.com>
2024-01-12 14:47:09 +0100

    Modal functionality added
    1) Added onClose property to Global element (window) by using:
    	1)  useRef (created modelRef start @ "null" than "existing" when clicking on target (cart));
    	2)  useEffect to settle outside modal click event by checking (if useRef exist & if click is not on the Modal (with
    contains(event.target as Node) & by creating a listener on window element than by wash away the listener after clicking
    outside
    
    2) Added the useState on main App.tsx to handle the click that makes the Modal disappear

--- site-e-commerce/src/App.tsx
+++ site-e-commerce/src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
 import Header from './components/Header/Header';
+import { useState } from 'react';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <>
       <Header />
+      {showModal && (
+        <div
+          className="modal-overlay"
+          onClick={() => {
+            setShowModal(false);
+          }}
+        >
+          {/* Vous pouvez également afficher un message comme "Cliquez pour fermer" ici */}
+        </div>
+      )}
     </>
   );
 }
--- site-e-commerce/src/components/header/Header_Components/Modal/Modal.tsx
+++ site-e-commerce/src/components/header/Header_Components/Modal/Modal.tsx
@@ -1,13 +1,34 @@
 import PropTypes from 'prop-types';
+import { useRef, useEffect } from 'react';
 
 interface ModalProps {
   onClose: () => void;
 }
 
 export default function Modal({ onClose }: ModalProps) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      // on vérifie si Modal existe et si le click n'est pas réalisé sur la Modal même
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [onClose]);
+
   return (
     <div className="modal">
-      <div>I am a modal dialog</div>
+      <div ref={modalRef}>I am a modal dialog</div>
       <button onClick={onClose}>Close</button>
     </div>
   );
