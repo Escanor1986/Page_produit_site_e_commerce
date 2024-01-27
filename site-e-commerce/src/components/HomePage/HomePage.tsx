6628106a9ca58f13ae6828e8872fca12ff9272c5
Escanor1986 <lionel.zovi@gmail.com>
2024-01-27 16:58:37 +0100

    added base of the carousel on homePage

--- site-e-commerce/src/components/HomePage/HomePage.tsx
+++ site-e-commerce/src/components/HomePage/HomePage.tsx
@@ -1,11 +1,14 @@
 import './HomePage.css';
 import ArticleComponent from './HomePage_Components/Articles/Articles';
+import { Carousel } from './HomePage_Components/Carousel/Carousel';
 
 function HomePage() {
   return (
     <>
       <main className="cart-wrapper flex flex-row">
-        <div className="left-cart-content  bg-orange-400"></div>
+        <div className="left-cart-content flex items-center">
+          <Carousel />
+        </div>
         <div className="right-cart-content">
           <ArticleComponent />
         </div>
---
+++ site-e-commerce/src/components/HomePage/HomePage_Components/Carousel/Carousel.tsx
@@ -0,0 +1,33 @@
+import './Carousel.css';
+import image1 from '../../../../assets/sneakers_1.webp';
+import image2 from '../../../../assets/sneakers_2.webp';
+import image3 from '../../../../assets/sneakers_3.webp';
+import image4 from '../../../../assets/sneakers_4.webp';
+
+export const Carousel = () => {
+  const images = [image1, image2, image3, image4];
+
+  const navImages = images.map((item, index) => (
+    <img
+      src={item}
+      key={index}
+      alt="Petite Image"
+      className="rounded-xl w-full"
+    />
+  ));
+
+  return (
+    <>
+      <div className="grid gap-4">
+        <div className="row-span-1">
+          <img
+            src={images[0]}
+            alt="Grande Image"
+            className="rounded-xl w-full"
+          />
+        </div>
+        <div className="grid grid-cols-4 gap-4">{navImages}</div>
+      </div>
+    </>
+  );
+};
