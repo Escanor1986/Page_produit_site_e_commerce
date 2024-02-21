2841e79ee07f3a6ed41b81da1123e8ff30a9d921
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-02-21 11:14:04 +0100

    Starting from scratch

--- api/app.ts
+++ api/app.ts
@@ -1,38 +1,14 @@
-import createError from "http-errors";
-import express, { Request, Response, NextFunction, Express } from "express";
-import path from "path";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express, { Express, Request, Response } from 'express'
 
-const app: Express = express();
+const app: Express = express()
 
-app.use(cors());
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
+const PORT = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello, world")
+})
 
-app.use(function (req: Request, res: Response, next: NextFunction) {
-  setTimeout(next, Math.floor(Math.random() * 2000 + 100));
-});
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`)
+})
 
-// catch 404 and forward to error handler
-app.use(function (req: Request, res: Response, next: NextFunction) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-export default app;
