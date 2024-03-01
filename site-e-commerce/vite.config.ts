8b25433aea9722a6fce87603c3d7fa8522e2baff
Thomas Dimnet <thomas.dimnet@gmail.com>
2024-03-01 17:46:44 +0100

    Revert changes

--- site-e-commerce/vite.config.ts
+++ site-e-commerce/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://escanor1986.github.io/Page_produit_site_e_commerce/',
+  base: '',
   plugins: [react()],
   // root: './', optionnel car il prend le répertoire courant, sinon erreur 404 vu que vite ne trouve pas le fichier racine
 });
