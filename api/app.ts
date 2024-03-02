065963637baaa90d29bd052e2a1cc148f7a5067a
Escanor1986 <lionel.zovi@gmail.com>
2024-03-02 09:23:21 +0100

    changes

--- api/app.ts
+++ api/app.ts
@@ -4,6 +4,14 @@ const app: Express = express()
 
 const PORT = 3000
 
+app.use(express.json());
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
 app.get("/", (req: Request, res: Response) => {
   res.send("hello, world")
 })
