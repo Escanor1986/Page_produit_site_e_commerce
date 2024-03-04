135a4bdcd4ef6ad3083544ec60604e8b233c30ee
Escanor1986 <lionel.zovi@gmail.com>
2024-03-04 10:06:55 +0100

    renamed app.tsx to App.tsx for build issues on github pages

--- site-e-commerce/src/main.tsx
+++ site-e-commerce/src/main.tsx
@@ -1,7 +1,7 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app';
+import App from './App';
 import { ThemeProvider } from './contexts/ThemeProvider';
 import { ArticleProvider } from './contexts/ArticleProvider';
 
