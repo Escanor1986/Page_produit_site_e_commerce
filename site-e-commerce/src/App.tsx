f5b7b0ecce48efc2c3eeba91f90921a4c8061542
Escanor1986 <lionel.zovi@gmail.com>
2024-03-04 10:03:54 +0100

    Changed BrowserRouter for HashRouter to try solving router issue and changed App.tsx to app.tsx

--- site-e-commerce/src/App.tsx
+++ site-e-commerce/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/Product';
 import ErrorPage from './pages/Error';
@@ -8,8 +8,8 @@ function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="dark:bg-slate-700 min-h-screen min-w-full flex flex-col justify-center items-center ">
-      <BrowserRouter>
+    <div className="dark:bg-slate-700 min-h-screen min-w-full flex flex-col justify-center items-center">
+      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -18,12 +18,10 @@ function App() {
         {showModal && (
           <div
             className="modal-overlay"
-            onClick={() => {
-              setShowModal(false);
-            }}
+            onClick={() => setShowModal(false)}
           ></div>
         )}
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
